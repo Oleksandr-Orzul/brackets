3a77e00f5689a9337466112e4ae2b0385bde35e1
Oleksandr <oleksandryurievich@gmail.com>
2023-01-25 18:23:25 +0200

    feat: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let list = [];
+  let openB = [];
+  let closeB = [];
+  let dict = {};
+  bracketsConfig.forEach(function (a) {
+    openB.push(a[0]);
+    closeB.push(a[1]);
+    dict[a[1]] = a[0];
+  });
+  for (let i = 0; i < str.length; i++) {
+    if (openB.includes(str[i]) && closeB.includes(str[i])) {
+      if (list[list.length - 1] === str[i]) {
+        list.pop();
+      } else {
+        list.push(str[i]);
+      }
+    } else if (openB.includes(str[i])) {
+      list.push(str[i]);
+    } else if (closeB.includes(str[i]) && list.length == 0) {
+      return false;
+    } else if (list[list.length - 1] === dict[str[i]]) {
+      list.pop();
+    }
+  }
+  console.log(list);
+  console.log(openB);
+  console.log(closeB);
+  console.log(dict);
+  return list.length === 0;
+};
